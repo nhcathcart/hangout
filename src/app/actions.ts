bd4af97cf7bb194263c004dfda5f98c6b3973902
Nicholas Cathcart <nhcathcart@gmail.com>
2024-01-18 21:27:31 -0500

    new models and auth logic complete. New posts are now associated with the current user by accessing their id from the jwt.

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,10 +1,13 @@
 "use server"
-
+import { auth } from "@/auth";
 import { prisma } from "@/server/db/client";
 
+export async function checkAuth() {
+  const session = await auth();
+  if (session?.user) return true;
+  return false;
+}
 export async function getAllPosts() {
   const res = await prisma.post.findMany();
-  console.log("res type is: ", typeof res)
-  console.log("res is: ", res)
   return res
 }
